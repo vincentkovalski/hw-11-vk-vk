e73ba0fbfbb1f87563a74574745b29b52b02d786
vincentkovalski <kovall89@gmail.com>
2022-05-30 15:06:55 +0500

    Initial Commit

---
+++ hw-11-vk-vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw-11-vk-vk
+//
+//  Created by Виктор Ковалевский on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
